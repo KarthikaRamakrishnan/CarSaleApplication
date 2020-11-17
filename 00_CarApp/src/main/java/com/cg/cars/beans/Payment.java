136d44024721654c8b83492e45e05d7c171dcc10
Jayasreec107 <74338336+Jayasreec107@users.noreply.github.com>
2020-11-17 09:28:49 +0530

    Update Payment.java

--- 00_CarApp/src/main/java/com/cg/cars/beans/Payment.java
+++ 00_CarApp/src/main/java/com/cg/cars/beans/Payment.java
@@ -64,4 +64,44 @@ public class Payment {
 		this.card = card;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((card == null) ? 0 : card.hashCode());
+		result = prime * result + (int) (paymentId ^ (paymentId >>> 32));
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		if (card == null) {
+			if (other.card != null)
+				return false;
+		} else if (!card.equals(other.card))
+			return false;
+		if (paymentId != other.paymentId)
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+	
 }
