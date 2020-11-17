d609532c8251ba5109b0c955e948a6e3744622ab
RukumbaiKokkula <74294949+RukumbaiKokkula@users.noreply.github.com>
2020-11-17 08:58:03 +0530

    Update ICarServiceImpl.java

--- 00_CarApp/src/main/java/com/cg/cars/service/ICarServiceImpl.java
+++ 00_CarApp/src/main/java/com/cg/cars/service/ICarServiceImpl.java
@@ -16,7 +16,9 @@ public class ICarServiceImpl implements ICarService{
 	}
 	@Override
 	public Car addCar(Car car) {
+		carRepository.beginTransaction();
 		car= carRepository.addCar(car);
+		carRepository.commitTransaction();
 		return car;
 		   
 	    }
@@ -25,46 +27,59 @@ public class ICarServiceImpl implements ICarService{
 	public Car removeCar(long id) {
 		Car car=null;
 		try {
-         car= carRepository.removeCar(id);
+		carRepository.beginTransaction();
+        	 car= carRepository.removeCar(id);
 		}catch(InvalidCarIdException e) {
 			e.printStackTrace();
 			e.getMessage();
+		carRepository.commitTransaction();
 		}
      return car;
 	}
 
 	@Override
 	public Car updateCar(long id, Car car) {
+	    carRepository.beginTransaction();
 	    carRepository.updateCar(id, car);
+	    carRepository.commitTransaction();
 		return car;
 	      
 	}
 
 	@Override
 	public Car getCar(long id) { 
+		carRepository.beginTransaction();
 		Car car=carRepository.getCar(id);
+		carRepository.commitTransaction();
 		return car;
 	}
 
 	@Override
 	public List<Car> getAllCars() {
-	      return carRepository.getAllCars();
+		carRepository.beginTransaction();
+	        return carRepository.getAllCars();
+		carRepository.commitTransaction();
 	}
 
 	@Override
 	public List<Car> getCarsByLocation() {
-		
+		carRepository.beginTransaction();
 		return carRepository.getCarsByLocation();
+		carRepository.commitTransaction();
 	}
 
 	@Override
 	public List<Car> getCarsByModel() {
+		carRepository.beginTransaction();
 		return carRepository.getCarsByLocation();
+		carRepository.commitTransaction();
 	}
 
 	@Override
 	public List<Car> getCarsByBrand() {
+		carRepository.beginTransaction();
 		return carRepository.getCarsByBrand();
+		carRepository.commitTransaction();
 	}
 
 }
